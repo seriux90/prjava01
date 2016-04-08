1eaed61d1074e7ef2fc9f7bd3cf71ba3467742da
alumne <alumne@alumne-PC>
2016-04-08 20:22:01 +0200

    Versió 0.1 del projecte prjava00

---
+++ prjava00/src/prjava00/inici.java
@@ -0,0 +1,10 @@
+package prjava00;
+
+public class inici {
+
+	public static void main(String[] args) {
+		System.out.println("Hola món");
+		System.out.println("Versió 0.1 del projecte prjava00");
+	}
+
+}
